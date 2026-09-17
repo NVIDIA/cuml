4e148bf38f6a66bfb13cac4da7448d650fe0fa4e
Hyunsu Cho <phcho@nvidia.com>
2026-09-16 18:09:25 -0700

    Revert "Remove extra newline"
    This reverts commit bd6b7d45e914287d2889d3f1fdeaae257126fd6d.

--- python/cuml/cuml/dask/ensemble/randomforestclassifier.py
+++ python/cuml/cuml/dask/ensemble/randomforestclassifier.py
@@ -51,6 +51,7 @@ class RandomForestClassifier(
         * If ``True``, each tree in the forest is built on a bootstrapped
           sample with replacement.
         * If ``False``, the whole dataset is used to build each tree.
+
         Weighted bootstrapping through ``sample_weight`` or ``class_weight``
         is not yet supported for distributed random forests.
     max_samples : float (default = 1.0)
--- python/cuml/cuml/dask/ensemble/randomforestregressor.py
+++ python/cuml/cuml/dask/ensemble/randomforestregressor.py
@@ -39,6 +39,7 @@ class RandomForestRegressor(
         * If ``True``, each tree in the forest is built on a bootstrapped
           sample with replacement.
         * If ``False``, the whole dataset is used to build each tree.
+
         Weighted bootstrapping through ``sample_weight`` is not yet supported
         for distributed random forests.
     max_samples : float (default = 1.0)
