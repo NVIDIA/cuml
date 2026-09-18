607fda442ac20ab9d0dbe55421e9421c256072a6
Simon Adorf <sadorf@nvidia.com>
2026-09-18 09:14:48 -0500

    Fix genetic namespace API docs

--- cpp/include/cuml/genetic/node.h
+++ cpp/include/cuml/genetic/node.h
@@ -143,7 +143,7 @@ struct node {
 
   /** node type */
   type t;
-  union {
+  union data {
     /**
      * if the node is `variable` type, then this is the column id to be used to
      * fetch its value, from the input dataset
